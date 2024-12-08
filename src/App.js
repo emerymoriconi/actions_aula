0f8eb1c7e98623a4fdad05ba61703a78f74e8fa5
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2024-12-08 15:04:07 -0300

    testing app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aulinha e tutorial.
+          Será se deu certo?
         </p>
         <a
           className="App-link"
